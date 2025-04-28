194b39ffe652847196679e8b5435a79d6c6c9a06
nikicha2209 <nikicha08@gmail.com>
2025-04-28 17:34:49 +0300

    Added the exercise

---
+++ Flat Rock Technology - Test task/Product.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace Flat_Rock_Technology___Test_task
+{
+    public class Product
+    {
+        [JsonProperty("productName")]
+        public string ProductName { get; set; }
+
+        [JsonProperty("price")]
+        public string Price { get; set; }
+
+        [JsonProperty("rating")]
+        public string Rating { get; set; }
+    }
+}
---
+++ Flat Rock Technology - Test task/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Globalization;
+using System.Web;
+using HtmlAgilityPack;
+using Newtonsoft.Json;
+
+namespace Flat_Rock_Technology___Test_task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string html = File.ReadAllText("input.html"); //load html file
+            HtmlDocument document = new HtmlDocument();
+            document.LoadHtml(html);
+
+            List<Product> products = new List<Product>();
+
+            foreach (HtmlNode itemDiv in document.DocumentNode.SelectNodes("//div[@class = \"item\"]")) //find all product elements
+            {
+                string ratingString = itemDiv.GetAttributeValue("rating", "0"); // get and normalize rating
+                if (!double.TryParse(ratingString, out double rating))
+                {
+                    rating = 0;
+                }
+
+                if (rating > 5)
+                {
+                    rating /= 2.0;
+                }
+
+                // get product name from image alt and price from span
+                var imgNode = itemDiv.SelectSingleNode(".//figure/a/img"); 
+                var priceNode = itemDiv.SelectSingleNode(".//p[@class=\"price\"]//span[@itemprop=\"price\"]//span[@style=\"display: none\"]");
+
+                if (imgNode != null && priceNode != null)
+                {
+                    string productName = HttpUtility.HtmlDecode(imgNode.GetAttributeValue("alt", "").Trim());
+                    string priceText = priceNode.InnerText.Replace("$", "").Trim();
+
+                    string formattedRating = rating % 1 == 0 ? ((int)rating).ToString() : rating.ToString("0.0", CultureInfo.InvariantCulture); // format rating properly
+
+                    products.Add(new Product
+                    {
+                        ProductName = productName,
+                        Price = priceText,
+                        Rating = formattedRating
+                    });
+                }
+
+            }
+
+            //output json
+            string json = JsonConvert.SerializeObject(products, Formatting.Indented);
+            Console.WriteLine(json);
+        }
+    }
+}
